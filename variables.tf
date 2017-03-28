8cd73858c4d5573e12b024b18e03e80f7db145df
Ambes Stephen <anbestephen@gmail.com>
2017-03-28 15:48:15 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  description = "The AWS access key id credential" 
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret access key credential" 
+} 
+
+variable "aws_account" {
+  description = "The AWS Account we are building infrastructure in." 
+} 
+
+variable "aws_region" {
+  description = "The AWS Region we are building infrastructure in." 
+}
